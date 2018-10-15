78ab4cfc59e2e53fdead824ca487c843cf372b2d
Ilya Kosarev <ilyanapar@yandex.ru>
2018-10-15 04:57:16 +0300

    Fixes

--- main.cpp
+++ main.cpp
@@ -26,6 +26,35 @@ double scalar_product(const vector<double> &vec1, const vector<double> &vec2)
 
 }
 
+int axpby(vector<double> &vec1, const vector<double> &vec2, double a, double b)
+{
+    const size_t ss = (vec1.size() / 4) * 4;
+    // float time = omp_get_wtime();
+
+//#pragma omp parallel for
+    for (size_t i = 0; i < ss; i += 4)
+    {
+        vec1[i] = a * vec1[i] + b * vec2[i];
+        vec1[i + 1] = a * vec1[i + 1] + b * vec2[i + 1];
+        vec1[i + 2] = a * vec1[i + 2] + b * vec2[i + 2];
+        vec1[i + 3] = a * vec1[i + 3] + b * vec2[i + 3];
+    }
+
+    // #pragma omp parallel for
+    for (size_t i = ss; i < vec1.size(); i++)
+        vec1[i] = a * vec1[i] + b * vec2[i];
+
+    // #pragma omp parallel for
+    // for (int i = 0; i < vec1.size; i++)
+    //     vec1[i] = a * vec1[i] + b * vec2[i];
+
+    // time = omp_get_wtime() - time;
+    // globtime += time;
+    // cout << "> Time of computation = " << (time) << endl;
+
+    return 0;
+}
+
 vector<double> multiplication(const Matrix &matrix, const vector<double> &vec)
 {
     c = new double[rowsA];
@@ -101,52 +130,74 @@ struct result
     int nit;
 };
 
-result solver(const Matrix &matrix, const vector<double> &right_part, result &data)
+void solver(const Matrix &matrix, const vector<double> &right_part, result &data)
 {
+    Matrix DD(matrix);
     vector<double> temp_right_part1 = right_part;
     vector<double> temp_right_part2 = right_part;
+    vector<double> PP(right_part.size(), 0), VV(right_part.size(), 0);
     double initres = sqrt(scalar_product(right_part, right_part));
     double eps = max(numeric_limits<double>::min(), data.tol * initres);
     double res = initres;
-    for(int i = 0; i < data.maxit; i++) {
+    double alphai_1 = 1,
+           betai_1  = 1,
+           Rhoi_2 = 1,
+           wi_1 = 1;
+    int i = 0;
+    data.res = vector<double>(right_part.size(), 0);
+    for (i = 0; i < data.maxit; i++) {
         printf("It %d: res = %e; tol = %e\n", i, res, res / initres);
-        if(res<eps) break;
-        if(res>initres/mineps) return -1;
-        if(i==0) Rhoi_1 = initres*initres;
-        else Rhoi_1 = dot(RR2,RR);
-        if(fabs(Rhoi_1)<RhoMin) return -1
-        if(i==0) PP=RR;
-        else{
-            betai_1=(Rhoi_1*alphai_1)/(Rhoi_2*wi_1);
-            axpby(PP, RR, betai_1, 1.0); // p=r+betai_1*(p-w1*v)
-            axpby(PP, VV, 1.0, -wi_1*betai_1);
+        if (res < eps) break;
+        if (res > initres / numeric_limits<double>::min()) {
+            data.nit = -1;
+            return;
         }
-        SpMV(DD,PP,PP2);
-        SpMV(A,PP2,VV);
-        alphai = dot(RR2,VV);
-        if(fabs(alphai)<RhoMin) return -3;
-        alphai = Rhoi_1/alphai;
-        SS=RR; // s=r-alphai*v
+        double Rhoi_1 = initres * initres;
+        if (i) Rhoi_1 = scalar_product(temp_right_part2, temp_right_part1);
+        if (abs(Rhoi_1) < numeric_limits<double>::min()) {
+            data.nit = -1;
+            return;
+        }
+        if (i) {
+            betai_1 = (Rhoi_1 *  alphai_1) / (Rhoi_2 * wi_1);
+            axpby(PP, temp_right_part1, betai_1, 1.0); //p = r + betai_1 * (p - w1 * v)
+            axpby(PP, VV, 1.0, -wi_1 * betai_1);
+        }
+        vector<double> PP2 = multiplication(DD, PP);
+        VV = multiplication(matrix, PP2);
+        double alphai = scalar_product(temp_right_part2, VV);
+        if (abs(alphai) < numeric_limits<double>::min()) {
+            data.nit = -3;
+            return;
+        }
+        alphai = Rhoi_1 / alphai;
+        vector<double> SS = temp_right_part1; //s = r - alphai * v
         axpby(SS, VV, 1.0, -alphai);
-        SpMV(DD, SS, SS2);
-        SpMV(A, SS2, TT);
-        wi = dot(TT, TT);
-        if(fabs(wi)<RhoMin) return -4;
-        wi = dot(TT, SS) / wi;
-        if(fabs(wi)<RhoMin) return -5;
-        // x=x+alphai*p2+wi*s2
-        axpby(XX, PP2,1.0,alphai);
-        axpby(XX, SS2,1.0,wi);
-        RR=SS; // r=s-wi*t
-        9
-        axpby(RR, TT, 1.0, -wi);
-        alphai_1=alphai;
-        Rhoi_2=Rhoi_1;
-        wi_1=wi;
-        res = sqrt(dot(RR,RR));
+        vector<double > SS2 = multiplication(DD, SS);
+        vector<double> TT = multiplication(matrix, SS2);
+        double wi = scalar_product(TT, TT);
+        if (abs(wi) < numeric_limits<double>::min()) {
+            data.nit = -4;
+            return;
+        }
+        wi = scalar_product(TT, SS) / wi;
+        if (abs(wi) < numeric_limits<double>::min()) {
+            data.nit = -5;
+            return;
+        }
+        //x = x + alphai * p2 + wi * s2
+        axpby(data.res, PP2,1.0,alphai);
+        axpby(data.res, SS2,1.0,wi);
+        temp_right_part1 = SS; //r = s - wi * t
+        axpby(temp_right_part1, TT, 1.0, -wi);
+        alphai_1 = alphai;
+        Rhoi_2 = Rhoi_1;
+        wi_1 = wi;
+        res = sqrt(scalar_product(temp_right_part1, temp_right_part1));
     }
-    if(info) printf("Solver_BiCGSTAB: outres: %g\n",res);
-    return I;
+    printf("Solver_BiCGSTAB: outres: %g\n", res);
+
+    data.nit = i;
 }
 
 int main()
